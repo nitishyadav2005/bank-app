0afb1fd16061228646a36cdd2a57b9530125088c
Nitish Yadav <nitishyadav1817@gmail.com>
2026-07-08 10:22:56 +0530

    git inti

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,30 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
-    
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        System.out.println("Welcome to Console Bank");
+        while(running) {
+            System.out.println("""
+                    1) Open Account
+                    2) Deposit
+                    3) Withdraw
+                    4) Transfer
+                    5) Account Statement
+                    6) List Accounts
+                    7) Search Accounts by Customer
+                    0) Exit
+                    """);
+            System.out.print("CHOOSE: ");
+            String choice = scanner.nextLine().trim();
+            System.out.println("CHOICE: " + choice);
+
+            switch (choice) {
+                case "0" -> running = false;
+            }
+        }
+    }
 }
